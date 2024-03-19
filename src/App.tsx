41f26032c657617840797722e385c6ffc390a23d
Ryan Ziegler <rzig408@gmail.com>
2024-03-18 19:08:32 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useRef } from 'react';
 import './App.css';
 
+function downloadURI(uri: string, name: string) {
+  var link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+
 function App() {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const pdfLatex = useRef<any>(null);
+  useEffect(() => {
+    // @ts-ignore
+    pdfLatex.current = new PDFTeX();
+  }, []);
+  const compile = async () => {
+    const latexBody = textareaRef.current?.value;
+    if(pdfLatex.current == null) {
+      return;
+    }
+    await pdfLatex.current.set_TOTAL_MEMORY(80*1024*1024);
+    const dataurl = await pdfLatex.current.compile(latexBody);
+    downloadURI(dataurl, "file.pdf");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Enter latex here:<br/>
+        <textarea style={{width: 500, height: 1000}} ref={textareaRef}>
+          
+        </textarea><br/>
+        <button onClick={compile}>Download</button>
     </div>
   );
 }
